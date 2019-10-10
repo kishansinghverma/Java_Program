6934d7d55603be5664f626d285e9424cd1f80eeb
kishansinghverma <kishansinghverma@gmail.com>
2019-10-11 00:03:31 +0530

    init

---
+++ src/string.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class string {
+
+    public static void main(String args[]) {
+        /*
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        char ch=sc.next().charAt(0);
+        char []c=str.toCharArray();
+        System.out.println(c[0]);
+        System.out.println(ch);
+      */
+
+        String str="My name is Kishan";
+        System.out.println(str.charAt(3));
+        char arr[]=str.toCharArray();
+        System.out.println(arr[5]);
+    }
+
+}
